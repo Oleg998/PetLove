e0fb63b604ae77ad295d061afbd3895eed1faf83
Oleg <trussameto@gmail.com>
2025-03-11 01:55:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-
+import { webcrypto } from 'crypto';
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
 export default defineConfig({
   base: "/petlove/",
   plugins: [react()],
