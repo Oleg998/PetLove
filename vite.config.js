d1f16150e17216d81105787dd56505ad77256c22
Oleg <trussameto@gmail.com>
2025-03-11 02:11:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/petlove/",
   plugins: [react()],
-
-  build: {
-    target: "esnext", 
-  },
   server: {
-    port: 3000, 
-    open: true,
+    port: 5173, // Налаштовуємо порт для локального сервера
+    open: true, // Автоматично відкривати браузер при старті сервера
+  },
+  build: {
+    outDir: 'dist', // Директорія для збірки
+    sourcemap: true, // Включає source maps для зручної налагодження
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Додаємо псевдонім для зручного імпорту
+    },
   },
 });
