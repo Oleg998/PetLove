ede4b14fc365e036efade98c54fc53b958b57f12
Oleg <trussameto@gmail.com>
2025-03-11 02:17:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/petlove/',
   plugins: [react()],
   server: {
     port: 5173, // Налаштовуємо порт для локального сервера
