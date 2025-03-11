ec051a15ceb7b111b6d02b762ac90f5e74bb02fb
Oleg <trussameto@gmail.com>
2025-03-11 08:26:51 +0200

    Update HomePage.jsx

--- src/components/Page/HomePage/HomePage.jsx
+++ src/components/Page/HomePage/HomePage.jsx
@@ -1,6 +1,9 @@
 import Navbar from "../../Navbar/Navbar";
 import style from "./home-page.module.css";
+
 const HomePage = () => {
+  const basePath = import.meta.env.BASE_URL; 
+
   return (
     <>
       <Navbar />
@@ -15,26 +18,26 @@ const HomePage = () => {
             your life with immeasurable joy and tenderness.
           </p>
         </div>
-        <div className={style.img_container} >
+        <div className={style.img_container}>
           <picture>
             <source
-              srcSet="/img/hero/heroMob.jpg 1x, /img/hero/heroMob@2.jpg 2x"
+              srcSet={`${basePath}img/hero/heroMob.jpg 1x, ${basePath}img/hero/heroMob@2.jpg 2x`}
               media="(max-width: 768px)"
               loading="lazy"
             />
             <source
-              srcSet="/img/hero/heroTab.jpg 1x, /img/hero/heroTab@2.jpg 2x"
+              srcSet={`${basePath}img/hero/heroTab.jpg 1x, ${basePath}img/hero/heroTab@2.jpg 2x`}
               media="(min-width: 769px) and (max-width: 1280px)"
               loading="lazy"
             />
             <source
-              srcSet="/img/hero/heroDesk.jpg 1x, /img/hero/heroDesk@2.jpg 2x"
+              srcSet={`${basePath}img/hero/heroDesk.jpg 1x, ${basePath}img/hero/heroDesk@2.jpg 2x`}
               media="(min-width: 1281px)"
               loading="lazy"
             />
             <img
-              src="/img/hero/heroMob.jpg"
-              srcSet="/img/hero/heroMob@2.jpg 2x"
+              src={`${basePath}img/hero/heroMob.jpg`}
+              srcSet={`${basePath}img/hero/heroMob@2.jpg 2x`}
               alt="hero_img"
               loading="lazy"
             />
