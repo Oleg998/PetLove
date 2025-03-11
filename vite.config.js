734c99270bcd35565b8541057e4891b926c84971
Oleg <trussameto@gmail.com>
2025-03-11 02:08:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { webcrypto } from 'crypto';
-if (!globalThis.crypto) {
-  globalThis.crypto = webcrypto;
-}
+
 export default defineConfig({
   base: "/petlove/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify", 
-    },
-  },
+
   build: {
     target: "esnext", 
   },
